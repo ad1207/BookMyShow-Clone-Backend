b24dc71e27d17a8e2fd90b6d1334b36df2863266
Adithya R <rsadithya12@gmail.com>
2022-07-11 19:09:23 +0530

    sd

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 mongo.connect()
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}))
 app.use(cookieParser());
 
 app.get("/",(req,res) => {
